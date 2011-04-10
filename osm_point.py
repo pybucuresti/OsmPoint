866dd543cbe8971f89ea4173b0d99b89e8183d7d
Alex Morega <alex@grep.ro>
2011-04-10 14:10:58 +0300

    make the form actually do something

--- osm_point.py
+++ osm_point.py
@@ -1,9 +1,19 @@
+DEBUG = True
+
 import flask
 app = flask.Flask(__name__)
+app.config.from_object(__name__)
 
 @app.route("/")
 def hello():
     return flask.render_template('home.html')
+
+@app.route("/save_poi", methods=['POST'])
+def save_poi():
+    app.logger.debug('something happened')
+    app.logger.debug("%r", flask.request)
+    print flask.request.form["name"]
+    return "whatever"
 
 def main():
     app.run(host='0.0.0.0')
