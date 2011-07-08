2d7e8f61c8fcad77d2500f0d472a4a7b769fb29d
Alex Morega <alex@grep.ro>
2011-07-08 18:45:57 +0300

    split js code into on-demand functions

--- static/js/frontend.js
+++ static/js/frontend.js
@@ -1,11 +1,13 @@
-$(function() {
-  window.M = {};
-  $('#map').empty();
-  M.proj_wgs1984 = new OpenLayers.Projection("EPSG:4326");
-  M.proj_mercator = new OpenLayers.Projection("EPSG:900913");
-  M.project = function(point) {
-    return point.clone().transform(M.proj_wgs1984, M.proj_mercator);
-  };
+(function() {
+
+window.M = {};
+M.proj_wgs1984 = new OpenLayers.Projection("EPSG:4326");
+M.proj_mercator = new OpenLayers.Projection("EPSG:900913");
+M.project = function(point) {
+  return point.clone().transform(M.proj_wgs1984, M.proj_mercator);
+};
+
+M.init_map = function() {
   M.map = new OpenLayers.Map({
     'div': "map",
     'controls': [
@@ -18,7 +20,19 @@ $(function() {
   }));
   M.map.addLayer(new OpenLayers.Layer.OSM());
   M.map.setCenter(M.project(new OpenLayers.LonLat(26.10, 44.43)), 13);
+}
 
+M.center_to_gps = function() {
+  window.navigator.geolocation.getCurrentPosition(function(position){
+      var lon = position.coords.longitude, lat = position.coords.latitude;
+      var center = new OpenLayers.LonLat(lon, lat);
+      M.map.setCenter(M.project(center), 16);
+      $('#poi-form form input[name=lat]').val(lat);
+      $('#poi-form form input[name=lon]').val(lon);
+  });
+}
+
+M.enable_adding_points = function() {
   var points_layer = new OpenLayers.Layer.Vector("Points");
   M.map.addLayer(points_layer);
 
@@ -29,14 +43,6 @@ $(function() {
   );
   M.map.addControl(draw_control);
   draw_control.activate();
-});
+};
 
-$(function() {
-  window.navigator.geolocation.getCurrentPosition(function(position){
-      var lon = position.coords.longitude, lat = position.coords.latitude;
-      var center = new OpenLayers.LonLat(lon, lat);
-      M.map.setCenter(M.project(center), 16);
-      $('#poi-form form input[name=lat]').val(lat);
-      $('#poi-form form input[name=lon]').val(lon);
-  });
-});
+})();
