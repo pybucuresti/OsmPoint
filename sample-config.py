265d0ae531198738728995ab1d2c4b741395a1c0
Alex Morega <alex@grep.ro>
2011-07-19 09:30:56 +0300

    rename sample config file; change some defaults

--- sample-config.py
+++ sample-config.py
@@ -1,17 +1,16 @@
 import os
-workdir = os.path.abspath(os.environ['OSMPOINT_WORKDIR'])
+import logging
+
+workdir = os.path.dirname(__file__)
 
 OPENID_FS_STORE_PATH = os.path.join(workdir, 'openid_store')
 OSM_PASSWORD_PATH = os.path.join(workdir,'osm-login.txt')
 SQLALCHEMY_DATABASE_URI = "sqlite:///%s" % os.path.join(workdir, 'db.sqlite3')
 
-try:
-    with open(os.path.join(workdir, 'secret'), 'rb') as f:
-        SECRET_KEY = f.read()
-except IOError:
-    SECRET_KEY = None # TODO issue a warning in the log
+#SECRET_KEY = "replace-with-a-random-string"
 
 DEBUG = True
 STATIC_CACHE_TIMEOUT = 0
 OSMPOINT_ADMINS = []
 
+logging.basicConfig()
